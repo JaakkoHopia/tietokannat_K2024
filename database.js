b32e77a9682e92f01101046448de5942db7f94a9
JaakkoHopia <156876814+JaakkoHopia@users.noreply.github.com>
2024-03-01 05:39:28 +0200

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'netuser',
-  password: 'netpassword321',
-  database: 'netdb'
-});
-module.exports = connection;
